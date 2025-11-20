cf799677fd5fee15e0882ce25d3ef0417fdbcb16
Nakul Ravindra Padalkar <2807210+nakulpadalkar@users.noreply.github.com>
2025-11-19 19:54:34 -0500

    added utterance to index

---
+++ theme/utterance.js
@@ -0,0 +1,8 @@
+<script src="https://utteranc.es/client.js"
+        repo="BostonAnalytics/AD698-generative-ai-for-BA"
+        issue-term="pathname"
+        label="utterance"
+        theme="github-light"
+        crossorigin="anonymous"
+        async>
+</script>
